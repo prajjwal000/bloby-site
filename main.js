44e3325dff5c1a5c768fac579dddc39b90e07b33
Prajjwal <prajjwalverma74@gmail.com>
2024-02-12 01:21:44 +0530

    Level2 added a trailer

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 import './style.css'
+
+const trailer = document.getElementById("trailer");
+
+window.onmousemove = e => {
+    const x = e.clientX - trailer.offsetWidth/2,
+          y = e.clientY - trailer.offsetHeight/2;
+    trailer.style.transform = `translate(${x}px, ${y}px)`;
+}
