30ef79a9c081281a180cdc35b7c6739de8ca8265
Prajjwal Verma <148757410+prajjwal000@users.noreply.github.com>
2024-02-12 10:29:28 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const trailer = document.getElementById("trailer");
 
 window.onmousemove = e => {
-    const x = e.clientX - trailer.offsetWidth/2,
-          y = e.clientY - trailer.offsetHeight/2;
+    const x = e.clientX - (trailer.offsetWidth)/2,
+          y = e.clientY - (trailer.offsetHeight)/2;
     trailer.style.transform = `translate(${x}px, ${y}px)`;
 }
